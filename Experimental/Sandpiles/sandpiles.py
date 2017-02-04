c627e727ac2a5448312b906b83999b5d85c00433
redundnt <redundnt@gmail.com>
2017-02-04 01:31:21 -0500

    Updates to sandpiles

--- Experimental/Sandpiles/sandpiles.py
+++ Experimental/Sandpiles/sandpiles.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python
 
 from random import randrange
-from time import time
+from time import time, sleep
 from nonblockingconsole import NonBlockingConsole as NBC
 from argparse import ArgumentParser
 
-#### FOR ANIMATION
+#### COLOR STUFF
+
+COLOR_RED = "\x1b[31m"
+COLOR_BLUE = "\x1b[34m"
+COLOR_GREEN = "\x1b[32m"
+COLOR_YELLOW = "\x1b[33m"
+COLOR_WHITE = "\x1b[37m"
+
+COLOR_NORMAL = "\x1b[0m"
+
+# Animation Options
+ANIMATE_PIXEL_COURSENESS = 10     # How many pixels per cell? 10 x 10
+ANIMATE_TIME_COURSENESS  = 10     # How many topels per frame? 10
+ANIMATE_FPS              = 20     # Frames Per Second
+ANIMATE_DURATION         = 20     # How many seconds do we play for?
+ANIMATE_OUTFILE          = lambda : "sp_{}.mp4".format(str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).split('.')[0])
+
+# Global Options
+START_CONFIG             = 'center10000'
+WIDTH                    = 250
+HEIGHT                   = 250
+PRETTY_PRINT             = False
+SLEEP_TIME               = 0.15
+RANDOM_ADD               = 3
+COLOR_PRINT              = False
+
+# FOR ANIMATION
 
 we_have_numpy    = False
 we_have_scipy    = False
@@ -21,15 +47,6 @@ try:
     we_have_moviepy = True
     import datetime
     
-    # Animation Options
-    ANIMATE_PIXEL_COURSENESS = 10     # How many pixels per cell? 10 x 10
-    ANIMATE_TIME_COURSENESS  = 10     # How many topels per frame? 10
-    ANIMATE_FPS              = 20     # Frames Per Second
-    ANIMATE_DURATION         = 20     # How many seconds do we play for?
-    ANIMATE_OUTFILE          = lambda : "sp_{}.mp4".format(str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).split('.')[0])
-    START_CONFIG             = 'center10000'
-    WIDTH                    = 250
-    HEIGHT                   = 250
 
 except Exception as e:
     pass
@@ -37,7 +54,7 @@ except Exception as e:
 class Sandpile(object):
     _default_topology = 'table'
     _topologies = ('table', 'torus', 'plane', 'projective-plane', 'mobius-band')
-    _characters = ' -*^%$#'
+    _characters = ' -*#$%abcdefghijklmnopqrstuvwxyz'
     print_intermediate = False
     _pretty_print      = False
     _print_sleep_time  = 0.04    # Smooth printing when printing quickly!
@@ -63,6 +80,10 @@ class Sandpile(object):
         if pretty_print in (True, False):
             Sandpile._pretty_print = pretty_print     # Print nice chars or just ints?
         self.pretty_print = Sandpile._pretty_print
+        # XXX: Hack
+        if COLOR_PRINT:
+            self._characters = (' ', COLOR_BLUE + '-' + COLOR_NORMAL, COLOR_GREEN + '*' + COLOR_NORMAL, 
+                                COLOR_YELLOW + '#' + COLOR_NORMAL, COLOR_RED + '!' + COLOR_NORMAL)
 
 
         # Create function to reinitialize
@@ -83,7 +104,7 @@ class Sandpile(object):
         elif isinstance(start_config, str):
             if start_config == 'random':
                 def f(x,y):
-                    return randrange(4)
+                    return randrange(5)
                 self.start_config = f        
             elif start_config == 'center':
                 def f(x,y):
@@ -101,6 +122,7 @@ class Sandpile(object):
                         return n
                     return 0
                 self.start_config = f        
+
             elif start_config.startswith('tower'):
                 config_str = start_config[5:]
                 configs = config_str.split(',')
@@ -114,6 +136,12 @@ class Sandpile(object):
                     return 0
                     
                 self.start_config = f        
+
+            elif start_config.isdigit():
+                val = int(start_config)
+                def f(x,y):
+                    return val
+                self.start_config = f
 
         # Check your sinks
         if sink_config == None:
@@ -262,18 +290,16 @@ class Sandpile(object):
         def sandpile_to_npimage(self):
             """ Converts the sandpile into a RGB image for the final video."""
                             # B G R
-            coefs = np.array([4,4,4]).reshape((3,1,1))
-            coloredpile = 15 * coefs * self.pile
+            coefs = np.array([80,15,40]).reshape((3,1,1))
+            coloredpile = coefs * self.pile
             image = coloredpile[::-1].swapaxes(0,2).swapaxes(0,1)
             return   np.minimum(255,  image)
 
         def make_frame(self, arg = None):
             pile_scale = ANIMATE_PIXEL_COURSENESS
-            for i in range(4):
+            for i in range(ANIMATE_TIME_COURSENESS):
                 self.topple_single()
             image = self.sandpile_to_npimage()
-            i = 0
-            ss = []
 
             result = np.zeros( (pile_scale * self.height, pile_scale * self.width, 3), dtype=int) # XXX: Changed
             for i in range(pile_scale * self.height):
@@ -309,7 +335,7 @@ class Sandpile(object):
                 print '\n' + ('='*80) + '\nEnter to continue'
                 while time() - self._print_sleep_time < t0:
                     if nbc.get_data():
-                        return
+                        return 'user_exit'
                 
     def setup(self):
         self.pile = np.zeros( (self.height, self.width), dtype=int) # XXX: Changed
@@ -347,11 +373,12 @@ class Sandpile(object):
                 while True:
                     yield
     
-    def add_random(self, n = 1):
+    def add_random(self, n = 1, auto_topple=True):
         for i in range(n):
             x,y = randrange(self.height), randrange( self.width)
             self.pile[x][y] += 1
-        self.topple()
+        if auto_topple:
+            self.topple()
         return self   # For chaining 
 
     def shift_up(self, n, filler_value = 0):
@@ -592,29 +619,43 @@ def tests():
     test6()
 
 
-def animate():
+def animate(args):
     s = Sandpile(START_CONFIG, width=WIDTH, height=HEIGHT, symmetric_topple = True)
+    if RANDOM_ADD != 3: # XXX: Hack, this is just dumb
+        s.add_random(RANDOM_ADD, auto_topple = False)
     s.create_animation(duration = ANIMATE_DURATION)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--mode",   type = str, default = 'normal', help = "modes include animate, test, and normal")
-    parser.add_argument("--width",  type = int, default = 250, help = "set width")
-    parser.add_argument("--height", type = int, default = 250, help = "set height")
-    parser.add_argument("--animate-pixel-courseness", type = int, default = 10,  help = "width/height in pixels per cell")
-    parser.add_argument("--animate-time-courseness",  type = int, default = 10, help = "topple iterations per frame")
-    parser.add_argument("--animate-fps",      type = int, default = 20, help = "frames per second for animation")
-    parser.add_argument("--animate-duration", type = int, default = 20, help = "duration in seconds")
-    parser.add_argument("--animate-filename",  type = str, default = '', help = "output file for animation")
-    parser.add_argument("--start-config",  type = str, default = 'center1000', help = "start configuration for sand pile")
-    parser.add_argument("--topology",  type = str, default = 'table', help = "topology we are working on")
+    parser.add_argument('-m', "--mode",   type = str, default = 'normal', help = "modes include animate, test, and normal")
+    parser.add_argument('-W', "--width",  type = int, default = 81, help = "set width")
+    parser.add_argument('-H', "--height", type = int, default = 81, help = "set height")
+    parser.add_argument('-sc', "--silent-compute", action = 'store_true', help = "don't print computation in normal mode")
+    parser.add_argument('-p', "--pretty",   action = 'store_true', help = "print pretty to terminal")
+    parser.add_argument('-apc', "--animate-pixel-courseness", type = int, default = 10,  help = "width/height in pixels per cell")
+    parser.add_argument('-atc', "--animate-time-courseness",  type = int, default = 10, help = "topple iterations per frame")
+    parser.add_argument('-fps', "--animate-fps",      type = int, default = 20, help = "frames per second for animation")
+    parser.add_argument('-ad', "--animate-duration", type = int, default = 20, help = "duration in seconds")
+    parser.add_argument('-af', "--animate-filename",  type = str, default = '', help = "output file for animation")
+    parser.add_argument('-S', "--start-config",  type = str, default = 'center1000', help = "start configuration for sand pile")
+    parser.add_argument('-t', "--topology",  type = str, default = 'table', help = "topology we are working on")
+    parser.add_argument('-s', "--sleep-time", type = float, default = 0.15, help = "time to sleep between dumping to screen")
+    parser.add_argument('-r', "--random-add", type = int, default = 3, help = "default number to randomly add")
+    parser.add_argument('-c', "--color-tty", action = 'store_true', help = "color tty output")
     args = parser.parse_args()
-
 
     HEIGHT                   = args.height
     WIDTH                    = args.width
+    PRETTY_PRINT             = args.pretty
+    START_CONFIG             = args.start_config
+    SLEEP_TIME               = args.sleep_time
+    RANDOM_ADD               = args.random_add
+    COLOR_PRINT              = args.color_tty
+
     if args.mode == 'normal':
-        pass
+        s = Sandpile(START_CONFIG, width=WIDTH, height=HEIGHT, topology = args.topology, pretty_print = PRETTY_PRINT, print_intermediate = not args.silent_compute)
+        s.topple()
+
     elif args.mode == 'animate':
         ANIMATE_DURATION = args.animate_duration
         ANIMATE_FPS      = args.animate_fps
@@ -622,7 +663,8 @@ if __name__ == '__main__':
         ANIMATE_TIME_COURSENESS  = args.animate_time_courseness
         ANIMATE_FILENAME         = args.animate_filename
         if raw_input("continue with animation? ").lower() in ('y', 'yes'):
-            animate()
+            animate(args)
+
     elif args.mode == 'test':
         print "mode =", args.mode
         print 'width =', args.width
@@ -635,3 +677,18 @@ if __name__ == '__main__':
         raw_input()
         tests()
         
+    elif args.mode == 'random':
+        s = Sandpile(START_CONFIG, width = WIDTH, height = HEIGHT, topology = args.topology, pretty_print = PRETTY_PRINT, print_intermediate = not args.silent_compute)
+        nbc = NBC()
+        
+        while True:
+            if nbc.get_data():
+                break
+            s.add_random(RANDOM_ADD, auto_topple = False)
+            if s.topple() == 'user_exit':
+                break
+            print s
+            print '\n' + ('='*80) + '\nEnter to continue'
+            sleep(SLEEP_TIME)
+            
+
