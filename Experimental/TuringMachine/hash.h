a5a2f8587fd17aa2bdf53257c326023b42b572fd
redundnt <redundnt@gmail.com>
2017-02-05 12:32:09 -0500

    Updated encoding for hastable_value_node_

--- Experimental/TuringMachine/hash.h
+++ Experimental/TuringMachine/hash.h
@@ -3,7 +3,30 @@
 
 #include <stdint.h>
 
-typedef uint64_t action;
+/* An action consists of:
+ *    - Optionally writing an element of our alphabet (0: write, 1: no write, 
+ *        8 bits for alphabet, total of 9 bits.
+ *    - Move R,L,S - 2 bits (00: no move; 10: left; 01: right; 11: undefined)
+ *
+ *    Since we have 8 bytes to work with, we can think of the following layout:
+ *
+ *    +-----------------+
+ *    | 1 bit for write |
+ *    +--------+--------+
+ *             |
+ *    +-------+++--+-----+--------+--------+--------+--------+--------+--------+
+ *    | ALPHA |b|  |     | SAME AS 1 AND 2 | SAME AS 1 AND 2 | SAME AS 1 AND 2 |
+ *    +-------+-+--+-----+--------+--------+--------+--------+--------+--------+
+ *     \_____/   \/ \___/
+ *        |       |   |___________________________
+ *        +-+     +-------+                      |
+ *          |             |                      |
+ * +--------+-------++----+------------++--------+--------+
+ * | 8 bit encoding || Two bits for    || 5 bits for move |
+ * | for alpha char || left/right/stay ||    multiplier   |
+ * +----------------++----+------------++-----------------+
+ */
+typedef uint64_t action_t;
 
 /* The hashtable key has 8 least significant bits to store alphabet, remaining
  * 56 most significant bits storing the state. 
@@ -19,8 +42,12 @@ typedef uint64_t Key;
  * */
 
 typedef struct hashtable_value_node_ {
-  ValueNode* next;  // Next value in the bucket (w/ same hash code)
-  uint64_t   state; // State to be transfered to - Extra 8 bits of storage here
+  ValueNode* next;   // Next value in the bucket (w/ same hash code)
+  uint64_t   state;  // State to be transfered to - Extra 8 bits of storage here
+  action_t   action; // What do we do? This is actually a bit more general than
+                     // a strict turing machine, but we have the extra space...
+                     // (in particular, our system will pad with extra space, so
+                     // lets use it...)
 } ValueNode;
 
 /*
